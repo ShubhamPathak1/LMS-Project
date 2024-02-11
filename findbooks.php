c4e7a3ff78814ca933b01b627dc061a630ac997f
Shubham <pathakshubhu123@gmail.com>
2024-02-11 22:17:12 +0545

    commented out filter sort features for now

--- findbooks.php
+++ findbooks.php
@@ -19,45 +19,45 @@
     </div> -->
     <div class="userotherPage">
         <h1 class="pageTitle">Find Books</h1>
-        <div class="filterSortBox">
+        <!-- <div class="filterSortBox">
             <input type="text" name="searchBookname" id="searchBookname" class="dataEntryInput searchBar" placeholder="Search Book">
                 <select name="categoriesList" id="categoriesList" class="selectItems dataEntryInput filterDrop">
                     <option value="addCatOptDefault" selected id="defaultCatSelect" class="selectOptions">Category</option>
                     <?php
-                $fetchCatQuery = "SELECT categoryname from category";
-                $fetchCatResult = mysqli_query($conn, $fetchCatQuery);
-                if (mysqli_num_rows($fetchCatResult) > 0) {;
-                    while ($row1 = mysqli_fetch_assoc($fetchCatResult)) {
-                        echo '<option class="selectOptions" value="' . $row1['categoryname'] . '">' . $row1['categoryname'] . '</option>';
-                    }
-                }
+                // $fetchCatQuery = "SELECT categoryname from category";
+                // $fetchCatResult = mysqli_query($conn, $fetchCatQuery);
+                // if (mysqli_num_rows($fetchCatResult) > 0) {;
+                //     while ($row1 = mysqli_fetch_assoc($fetchCatResult)) {
+                //         echo '<option class="selectOptions" value="' . $row1['categoryname'] . '">' . $row1['categoryname'] . '</option>';
+                //     }
+                // }
                 ?>
             </select>
             <select name="publicationsList" id="publicationsList" class="selectItems dataEntryInput filterDrop">
                 <option value="addPublicationOptDefault" selected id="defaultPublicationSelect" class="selectOptions">Add Publication</option>
                 <?php
-                $fetchPublicationQuery = "SELECT publicationname from publication";
-                $fetchPublicationResult = mysqli_query($conn, $fetchPublicationQuery);
-                if (mysqli_num_rows($fetchPublicationResult) > 0) {;
-                    while ($row3 = mysqli_fetch_assoc($fetchPublicationResult)) {
-                        echo '<option class="selectOptions" value="' . $row3['publicationname'] . '">' . $row3['publicationname'] . '</option>';
-                    }
-                }
+                // $fetchPublicationQuery = "SELECT publicationname from publication";
+                // $fetchPublicationResult = mysqli_query($conn, $fetchPublicationQuery);
+                // if (mysqli_num_rows($fetchPublicationResult) > 0) {;
+                //     while ($row3 = mysqli_fetch_assoc($fetchPublicationResult)) {
+                //         echo '<option class="selectOptions" value="' . $row3['publicationname'] . '">' . $row3['publicationname'] . '</option>';
+                //     }
+                // }
                 ?>
             </select>
             <select name="authorsList" id="authorsList" class="selectItems dataEntryInput filterDrop">
                 <option value="addAuthorOptDefault" selected id="defaultAuthorSelect" class="selectOptions">Author</option>
                 <?php
-                $fetchAuthorQuery = "SELECT authorname from author";
-                $fetchAuthorResult = mysqli_query($conn, $fetchAuthorQuery);
-                if (mysqli_num_rows($fetchAuthorResult) > 0) {;
-                    while ($row2 = mysqli_fetch_assoc($fetchAuthorResult)) {
-                        echo '<option class="selectOptions" value="' . $row2['authorname'] . '">' . $row2['authorname'] . '</option>';
-                    }
-                }
+                // $fetchAuthorQuery = "SELECT authorname from author";
+                // $fetchAuthorResult = mysqli_query($conn, $fetchAuthorQuery);
+                // if (mysqli_num_rows($fetchAuthorResult) > 0) {;
+                //     while ($row2 = mysqli_fetch_assoc($fetchAuthorResult)) {
+                //         echo '<option class="selectOptions" value="' . $row2['authorname'] . '">' . $row2['authorname'] . '</option>';
+                //     }
+                // }
                 ?>
             </select>
-        </div>
+        </div> -->
         <div class="displayPage">
             <?php
             $bookFetchSql = "SELECT books.bookid, books.bookname, books.booknumber, books.catid, category.categoryname, books.pubid, publication.publicationname, books.authorid, author.authorname, books.noofcopies, books.copiesavailable from books join category on books.catid=category.catid join publication on books.pubid=publication.pubid join author on books.authorid=author.authorid order by books.bookid";
