520cf116999b93ad58f1f0641eb7d98a69870169
Shubham <pathakshubhu123@gmail.com>
2024-02-12 17:55:28 +0545

    sql error resolved

--- sql/publicationtable.sql
+++ sql/publicationtable.sql
@@ -1,1 +1,1 @@
-CREATE TABLE publication (pubid int primary key AUTO_INCREMENT, publication varchar(100) not null) AUTO_INCREMENT=601;
+CREATE TABLE publication (pubid int primary key AUTO_INCREMENT, publicationname varchar(100) not null) AUTO_INCREMENT=601;
